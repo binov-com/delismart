0a99b1cf15206b30363bae6415bfa32923d881c4
binov-com <bessam.benyahya@gmail.com>
2017-09-10 19:27:18 +0200

    refactor : extract ProductCardComponent - product and product-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { UserService } from './user/user.service';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { DataTableModule } from "angular-4-data-table";
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { ProductFilterComponent } from './products/product-filter/product-filter
     AdminOrdersComponent,
     LoginComponent,
     ProductFormComponent,
-    ProductFilterComponent
+    ProductFilterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
