0115972a44e2f214720a3521bf22ff1c4134eb28
binov-com <bessam.benyahya@gmail.com>
2017-09-11 13:44:56 +0200

    display the number of shopping cart items in navbar [ bs badge ] - bs-navbar

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { ShoppingCartService } from './../shopping-cart/shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,12 +9,21 @@ import { AuthService } from "../auth/auth.service";
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit{
   appUser: AppUser;
+  shoppingCartItemCount: number;
 
-  constructor(private auth: AuthService, private router: Router) { 
-    auth.appUser$
-      .subscribe(appUser => this.appUser = appUser)
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) { }
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser)
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items) {
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+      }
+    });
   }
 
   logout() {
